cb0dc364945be270c0e371d5a33dc264a1241b47
jaeho <ovsoce@gmail.com>
2020-04-15 23:15:46 +0900

    Create validator2 lib

---
+++ validator2/src/androidTest/java/io/github/ovso/validator2/ExampleInstrumentedTest.kt
@@ -0,0 +1,26 @@
+package io.github.ovso.validator2
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@Suppress("NonAsciiCharacters")
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun `carValidatorTest`() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("io.github.ovso.validator2.test", appContext.packageName)
+        assertSame("41주8595".isVehicleNumber(appContext), true)
+    }
+}
