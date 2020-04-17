8843ec1d3a4be3baddea731052d59398d391177b
jaeho <ovsoce@gmail.com>
2020-04-17 13:31:31 +0900

    Validator and validator2 to merge

--- app/src/test/java/io/github/ovso/validator/ValidationTest.kt
+++ app/src/test/java/io/github/ovso/validator/ValidationTest.kt
@@ -1,5 +1,6 @@
 package io.github.ovso.validator
 
+import org.junit.Assert
 import org.junit.Test
 import java.util.regex.Pattern
 
@@ -14,6 +15,19 @@ class ValidationTest {
 
     @Test
     fun `차량 차대 번 유효성 검사`() {
-        assert("20너0821".isVehicleNumber())
+        Assert.assertSame("41주8595".isVehicleNumber(), true)
+        Assert.assertSame("부산27무6662".isVehicleNumber(), true)
+        Assert.assertSame("서울1나6881".isVehicleNumber(), true)
+        Assert.assertSame("02허9757".isVehicleNumber(), true)
+        Assert.assertSame("서울31사9141".isVehicleNumber(), true)
+        Assert.assertSame("서울01가1234".isVehicleNumber(), true)
+        Assert.assertSame("123가1234".isVehicleNumber(), true)
+        Assert.assertSame("대전50바4321".isVehicleNumber(), true)
+        Assert.assertSame("38국4104".isVehicleNumber(), true)
+        Assert.assertSame("38합4104".isVehicleNumber(), true)
+        Assert.assertSame("38육4104".isVehicleNumber(), true)
+        Assert.assertSame("38해4104".isVehicleNumber(), true)
+        Assert.assertSame("38공4104".isVehicleNumber(), true)
+
     }
 }
