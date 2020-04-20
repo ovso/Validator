2ba2163655d52f5fa4265301e7aea5abd5aa0534
jaeho <ovsoce@gmail.com>
2020-04-21 00:55:56 +0900

    Password test

--- validator/src/main/java/io/github/ovso/validator/PasswordExtension.kt
+++ validator/src/main/java/io/github/ovso/validator/PasswordExtension.kt
@@ -3,5 +3,11 @@ package io.github.ovso.validator
 import java.util.regex.Pattern
 
 fun String.isPassword(): Boolean {
+
+    val s = "(?!^[0-9]*$)(?!^[a-zA-Z]*$)^([a-zA-Z0-9]{6,15})$".toRegex()
+    return Pattern.compile("").matcher(this).matches()
+}
+fun String.isPassword2(): Boolean {
+    val s = "(?!^[0-9]*$)(?!^[a-zA-Z]*$)^([a-zA-Z0-9]{6,15})$".toRegex()
     return Pattern.compile("").matcher(this).matches()
 }
